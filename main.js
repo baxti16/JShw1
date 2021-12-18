48ef7327123180519816975ec1f37289dd23b86e
baxti16 <96297516+baxti16@users.noreply.github.com>
2021-12-18 17:37:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let name2 = 20;
+let name1 = 4;
+/* here we can find the sum, difference, multiplication, and division of these numbers by means of two variables */
+console.log('Первое число =' + name2);
+console.log('Второй число= ' + name1);
+let yigindi = name2 + name1;
+console.log('Подвела: ' + yigindi);
+// here we can see the sum of these numbers
+let ayirma = name2 - name1;
+console.log('Разделение: ' + ayirma);
+// here we can see the difference of these numbers
+let kopaytma = name2 * name1;
+console.log('Умножение: ' + kopaytma);
+// here we can see the multiplication of these numbers
+let bolinma = name2 / name1;
+console.log('Разделять: ' + bolinma);
+// here we can see the division of these numbers
